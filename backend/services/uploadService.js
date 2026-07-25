6554bb2aa0c2dfe77f42a0e548f2818b11b3c761
Divyashree Kottargi <divyashreekottargi@gmail.com>
2026-07-25 23:07:51 +0530

    Add uploads folder

--- backend/services/uploadService.js
+++ backend/services/uploadService.js
@@ -1,17 +1,20 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 
-const upload = multer({
-    storage: storage
-});
-
-module.exports = upload;
+module.exports = multer({ storage });
